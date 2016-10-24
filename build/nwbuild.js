5ed47015af5d3af557497e40b60d56da9d9316c8
brambles <qjnight@gmail.com>
2016-10-24 19:14:12 +0800

    modified nwbuild.js

--- build/nwbuild.js
+++ build/nwbuild.js
@@ -7,7 +7,7 @@ const build = {
   files: './tmp/build/**',
   // version: 'last',
   platforms: ['osx64', 'win32'], // win32, win64, osx32, osx64, linux32, linux64
-  flavor: 'sdk', // 'sdk', 'normal', 'flavor', 'nacl'
+  flavor: 'normal', // 'sdk', 'normal', 'flavor', 'nacl'
 
   buildType: 'versioned',
   buildDir: 'packages',
